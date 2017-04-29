eafa3b2da818b6eb247fc7275e6df99a655d1e41
Aleksandr, Borkun <alexandr.borkun1992@gmail.com>
2017-04-30 00:21:51 +0300

    add test

--- src/test/main/by/epam/kronos/jenkinsApi/app/Demo.java
+++ src/test/main/by/epam/kronos/jenkinsApi/app/Demo.java
@@ -14,6 +14,7 @@ import java.io.Writer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
 
 import com.google.common.io.Files;
 
@@ -29,7 +30,8 @@ public class Demo {
 	private static PrepareReportBuilder jr = new PrepareReportBuilder();
 	private static final String JOB_NAMES = PropertyProvider.getProperty("JOB_NAMES");
 	
-	public static void main(String[] args) throws IOException {
+	@Test
+	public void testOne() throws IOException {
 		
 		String [] lines = JOB_NAMES.split(" "); //Files.readAllLines(Paths.get(FILE_NAME));
 		for(String s: lines){
