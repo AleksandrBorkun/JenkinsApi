57f282ed9bf0b02aae854f87617633f419bf0889
Aleksandr, Borkun <alexandr.borkun1992@gmail.com>
2017-04-29 22:56:07 +0300

    "prepare for Jenkins. Ver 1.0"

--- src/main/java/by/epam/kronos/jenkinsApi/app/Demo.java
+++ src/main/java/by/epam/kronos/jenkinsApi/app/Demo.java
@@ -1,9 +1,9 @@
 package by.epam.kronos.jenkinsApi.app;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,17 +11,24 @@ import org.apache.logging.log4j.Logger;
 import by.epam.kronos.jenkinsApi.entity.JenkinsJobList;
 import by.epam.kronos.jenkinsApi.job.PrepareReportBuilder;
 import by.epam.kronos.jenkinsApi.parser.ExcelParser;
+import by.epam.kronos.jenkinsApi.property.PropertyProvider;
 import by.epam.kronos.jenkinsApi.utils.ReportNameMaker;
 
 public class Demo {
 	public static final Logger log = LogManager.getLogger(PrepareReportBuilder.class);
-	private static final String FILE_NAME = "src/main/resources/JobNames.txt";
+//	private static final String FILE_NAME = "src/main/resources/JobNames.txt";
 	private static PrepareReportBuilder jr = new PrepareReportBuilder();
+	private static final String JOB_NAMES = PropertyProvider.getProperty("JOB_NAMES");
 	
 	public static void main(String[] args) throws IOException {
-		List<String> lines = Files.readAllLines(Paths.get(FILE_NAME));
 		
-		for (String jobName: lines) {
+		String [] lines = JOB_NAMES.split(" "); //Files.readAllLines(Paths.get(FILE_NAME));
+		for(String s: lines){
+			System.out.println(s);
+			log.info(s);
+		}
+		
+	/*	for (String jobName: lines) {
 			String[] a = null;
 			if (jobName.contains("/")) {
 				a = jobName.split("/");
@@ -38,7 +45,7 @@ public class Demo {
 			log.info("Start writing the result to Excel file: " + ReportNameMaker.get());
 			ExcelParser.getInstance().writeReportToExcel();
 			log.info("Application close");
-		}
+		} */
 	}
 
 }
--- src/main/java/by/epam/kronos/jenkinsApi/property/PropertyProvider.java
+++ src/main/java/by/epam/kronos/jenkinsApi/property/PropertyProvider.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 
 public class PropertyProvider {
 
-	    private static final String CONFIG_PATH = "config";
+	    private static final String CONFIG_PATH = "variable";
 	    private static final ResourceBundle bundle = ResourceBundle.getBundle(CONFIG_PATH);
 
 	    public static String getProperty(String propertyName) {
